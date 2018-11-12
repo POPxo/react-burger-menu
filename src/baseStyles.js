61b20fad444c6bfe6e89e0e4cfcfc38eb9c93cd2
Shadab Khan <shadab.khan@popxo.com>
2018-11-12 17:49:23 +0530

    Remove default transform

--- src/baseStyles.js
+++ src/baseStyles.js
@@ -25,31 +25,6 @@ const styles = {
       zIndex: 1100,
       width,
       height: '100%',
-      MozTransform: isOpen
-        ? ''
-        : right
-          ? 'translate3d(100%, 0, 0)'
-          : 'translate3d(-100%, 0, 0)',
-      MsTransform: isOpen
-        ? ''
-        : right
-          ? 'translate3d(100%, 0, 0)'
-          : 'translate3d(-100%, 0, 0)',
-      OTransform: isOpen
-        ? ''
-        : right
-          ? 'translate3d(100%, 0, 0)'
-          : 'translate3d(-100%, 0, 0)',
-      WebkitTransform: isOpen
-        ? ''
-        : right
-          ? 'translate3d(100%, 0, 0)'
-          : 'translate3d(-100%, 0, 0)',
-      transform: isOpen
-        ? ''
-        : right
-          ? 'translate3d(100%, 0, 0)'
-          : 'translate3d(-100%, 0, 0)',
       transition: 'all 0.5s'
     };
   },
